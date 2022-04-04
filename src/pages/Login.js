1fb64340470fbcb1820d4161e13a8afb34f83003
Daniel Saraiva <danieldsvn@gmail.com>
2022-04-04 05:17:06 -0300

    Adiciona import de Proptypes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { setLogin } from '../redux/actions';
 
 export class Login extends React.Component {
