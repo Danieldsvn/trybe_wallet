489ba039d60bc12b7403c8a54e9f99db68dc818d
Daniel Saraiva <danieldsvn@gmail.com>
2022-04-04 00:12:39 -0300

    Cria rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
