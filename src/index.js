d029cfcdf892c90ede49b618951247badd9f653c
Daniel Saraiva <danieldsvn@gmail.com>
2022-04-04 11:30:06 -0300

    Corrige Browser router no app e instala propType

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -8,7 +9,9 @@ import store from './redux/store/index';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
