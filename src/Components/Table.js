3ab85b95f3ac9a01379bdbc6813448181ed61ad6
Daniel Saraiva <danieldsvn@gmail.com>
2022-04-11 04:59:53 -0300

    requisito 7 refatorado

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -36,38 +36,37 @@ export class Table extends React.Component {
   render() {
     const { expenses } = this.props;
     return (
-      <>
-        { expenses.map((expense) => (
-          <table key={ expense.id }>
-            <thead>
-              <tr>
-                <th>Descrição</th>
-                <th>Tag</th>
-                <th>Método de pagamento</th>
-                <th>Valor</th>
-                <th>Moeda</th>
-                <th>Câmbio utilizado</th>
-                <th>Valor convertido</th>
-                <th>Moeda de conversão</th>
-                <th>Editar/Excluir</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ this.getExpenseValue(expense) }</td>
-                <td>{ this.getCurrencyName(expense) }</td>
-                <td>{ this.getExchangeRate(expense) }</td>
-                <td>{ this.getConvertedValue(expense) }</td>
-                <td>Real</td>
-                <td> </td>
-              </tr>
-            </tbody>
-          </table>
-        ))}
-      </>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{ expense.description }</td>
+              <td>{ expense.tag }</td>
+              <td>{ expense.method }</td>
+              <td>{ this.getExpenseValue(expense) }</td>
+              <td>{ this.getCurrencyName(expense) }</td>
+              <td>{ this.getExchangeRate(expense) }</td>
+              <td>{ this.getConvertedValue(expense) }</td>
+              <td>Real</td>
+              <td> </td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
     );
   }
 }
